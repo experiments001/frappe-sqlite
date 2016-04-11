frappe/frappe

Removed / Replaced Celery related code

frappe/utils/doctor.py
@@ -1,150 +1,82 @@
 from __future__ import unicode_literals
-import json, base64, os
 import frappe.utils
-from frappe.celery_app import get_celery
-from frappe.utils.file_lock import check_lock, LockTimeoutError
+from collections import defaultdict
+from rq import Worker, Connection
+from frappe.utils.background_jobs import get_redis_conn, get_queue, get_queue_list, get_jobs
 from frappe.utils.scheduler import is_scheduler_disabled
-from collections import Counter
-from operator import itemgetter
 
-def get_redis_conn():
-	"Returns the redis connection that celery would use"
-	app = get_celery()
-	with app.connection() as conn:
-		r = conn.default_channel.client
-	return r
 
-def get_queues(site=None):
-	"Returns the name of queues where frappe enqueues tasks as per the configuration"
-	queues = ["celery"]
-	sites = [site] if site else frappe.utils.get_sites()
-	for site in sites:
-		queues.append(site)
-		queues.append('longjobs@' + site)
+def get_workers():
+	with Connection(get_redis_conn()):
+		workers = Worker.all()
+		return workers
 
-	return queues
 
-def get_task_body(taskstr):
-	return json.loads(base64.decodestring(json.loads(taskstr)['body']))
-
-def purge_pending_tasks(event=None, site=None):
+def purge_pending_tasks(event=None, site=None, queue=None):
 	"""
 	Purge tasks of the event event type. Passing 'all' will not purge all
 	events but of the all event type, ie. the ones that are enqueued every five
 	mintues and would any leave daily, hourly and weekly tasks
 	"""
-	r = get_redis_conn()
-
-	event_tasks = []
-	for app in frappe.get_all_apps(True):
-		all_events = frappe.get_hooks(app_name=app).get('scheduler_events', {})
-		if event:
-			event_tasks += all_events.get(event) or []
+	purged_task_count = 0
+	for queue in get_queue_list(queue):
+		q = get_queue(queue)
+		if (site and event):
+			for job in q.jobs:
+				if job.kwargs['site'] == site and job.kwargs['event'] == event:
+					job.delete()
+					purged_task_count+=1
+		elif site:
+			for job in q.jobs:
+				if job.kwargs['site'] == site:
+					job.delete()
+					purged_task_count+=1
+		elif event:
+			for job in q.jobs:
+				if job.kwargs['event'] == event:
+					job.delete()
+					purged_task_count+=1
 		else:
-			# tasks of all events
-			for tasks in all_events.values():
-				event_tasks += tasks or []
+			purged_task_count += q.count
+			q.empty()
 
-	count = 0
 
-	for queue in get_queues():
-		for taskstr in r.lrange(queue, 0, -1):
-			taskbody = get_task_body(taskstr)
-			kwargs = taskbody.get('kwargs')
-			if kwargs:
-				if site and kwargs.get('site') != site:
-					continue
+	return purged_task_count
 
-				if event:
-					if kwargs.get('handler') and kwargs.get('handler') in event_tasks:
-						r.lrem(queue, taskstr)
-						count += 1
-				else:
-					r.lrem(queue, taskstr)
-					count += 1
-	return count
+def get_jobs_by_queue(site=None):
+	jobs_per_queue = defaultdict(list)
+	queue_list = get_queue_list()
+	for queue in queue_list:
+		q = get_queue(queue)
+		for job in q.jobs:
+			if not site:
+				jobs_per_queue[queue].append(job.kwargs['method'])
+			elif job.kwargs['site'] == site:
+				jobs_per_queue[queue].append(job.kwargs['method'])
+		
 
-def get_pending_task_count():
+	return jobs_per_queue
+
+def get_pending_task_count(site=None, queue=None):
 	"Get count of pending tasks"
-	r = get_redis_conn()
 	pending = 0
-	for queue in get_queues():
-		pending += r.llen(queue)
+	for queue in get_queue_list(queue):
+		q = get_queue(queue)
+		for job in q.jobs:
+			if site is None:
+				pending += 1
+			else:
+				if job.kwargs[site] is site:
+					pending += 1
 	return pending
 
-def get_timedout_locks():
-	"Get list of stale locks from all sites"
-	old_locks=[]
-	for site in frappe.utils.get_sites():
-		locksdir = os.path.join(site, 'locks')
-		for lock in os.listdir(locksdir):
-			lock_path = os.path.join(locksdir, lock)
-			try:
-				check_lock(lock_path)
-			except LockTimeoutError:
-				old_locks.append(lock_path)
-	return old_locks
 
-def check_if_workers_online():
-	app = get_celery()
-	if app.control.inspect().ping():
-		return True
-	return False
-
-def dump_queue_status(site=None):
-	"""
-	Dumps pending events and tasks per queue
-	"""
-	ret = []
-	r = get_redis_conn()
-	for queue in get_queues(site=site):
-		if r.llen(queue):
-			queue_details = {
-				'queue': queue,
-				'len': r.llen(queue),
-			}
-			queue_details.update(get_task_count_for_queue(queue))
-			ret.append(queue_details)
-
-	ret = sorted(ret, key=itemgetter('len'), reverse=True)
-	ret.insert(0, {
-		'total': get_pending_task_count()
-	})
-	return ret
-
-def get_task_count_for_queue(queue):
-	"""
-	For a given queue, returns the count of every pending task and aggregate of
-	events pending
-	"""
-	r = get_redis_conn()
-	tasks = [get_task_body(taskstr) for taskstr in r.lrange(queue, 0, -1)]
-	task_names = [task['task'] for task in tasks]
-	task_counts = Counter(task_names)
-	event_counts = Counter(task['kwargs'].get('event') for task in tasks)
-	return {
-		'task_counts': task_counts,
-		'event_counts': event_counts
-	}
-
+def check_number_of_workers():
+	return len(get_workers())
+	
 def get_running_tasks():
-	ret = {}
-	app = get_celery()
-	inspect = app.control.inspect()
-	active = inspect.active()
-	if not active:
-		return []
-	for worker in active:
-		ret[worker] = []
-		for task in active[worker]:
-			ret[worker].append({
-				'id': task['id'],
-				'name': task['name'],
-				'routing_key': task['delivery_info']['routing_key'],
-				'args': task['args'],
-				'kwargs': task['kwargs']
-			})
-	return ret
+	for worker in get_workers():
+		return worker.get_current_job()
 
 
 def doctor():
@@ -152,12 +84,8 @@ def doctor():
 	Prints diagnostic information for the scheduler
 	"""
 	print "Inspecting workers and queues..."
-	workers_online = check_if_workers_online()
+	workers_online = check_number_of_workers()
 	pending_tasks = get_pending_task_count()
-
-	print "Finding locks..."
-	locks = get_timedout_locks()
-
 	print "Checking scheduler status..."
 	for site in frappe.utils.get_sites():
 		frappe.init(site)
@@ -168,25 +96,20 @@ def doctor():
 
 	print "Workers online:", workers_online
 	print "Pending tasks", pending_tasks
-	print "Timed out locks:"
-	print "\n".join(locks)
-	if (not workers_online) or (pending_tasks > 4000) or locks:
-		return 1
 
-	print "Note: To view pending tasks, use bench dump-queue-status"
 
 	return True
 
-def celery_doctor(site=None):
+def rq_doctor(site=None):
 	queues = dump_queue_status(site=site)
 	running_tasks = get_running_tasks()
 	print 'Queue Status'
 	print '------------'
-	print json.dumps(queues, indent=1)
+	print queues
 	print ''
 	print 'Running Tasks'
 	print '------------'
-	print json.dumps(running_tasks, indent=1)
+	print running_tasks
 
 def inspect_queue():
 	print 'Pending Tasks Queue'

experiments001/frappe-sqlite

frappe/utils/doctor.py
@@ -1,200 +1,127 @@
 from __future__ import unicode_literals
-import json, base64, os
 import frappe.utils
-from frappe.celery_app import get_celery
-from frappe.utils.file_lock import check_lock, LockTimeoutError
+from collections import defaultdict
+from rq import Worker, Connection
+from frappe.utils.background_jobs import get_redis_conn, get_queue, get_queue_list
 from frappe.utils.scheduler import is_scheduler_disabled
-from collections import Counter
-from operator import itemgetter
 
-def get_redis_conn():
-	"Returns the redis connection that celery would use"
-	app = get_celery()
-	with app.connection() as conn:
-		r = conn.default_channel.client
-	return r
 
-def get_queues(site=None):
-	"Returns the name of queues where frappe enqueues tasks as per the configuration"
-	queues = ["celery"]
-	sites = [site] if site else frappe.utils.get_sites()
-	for site in sites:
-		queues.append(site)
-		queues.append('longjobs@' + site)
+def get_workers():
+	with Connection(get_redis_conn()):
+		workers = Worker.all()
+		return workers
 
-	return queues
 
-def get_task_body(taskstr):
-	return json.loads(base64.decodestring(json.loads(taskstr)['body']))
-
-def purge_pending_tasks(event=None, site=None):
+def purge_pending_jobs(event=None, site=None, queue=None):
 	"""
 	Purge tasks of the event event type. Passing 'all' will not purge all
 	events but of the all event type, ie. the ones that are enqueued every five
 	mintues and would any leave daily, hourly and weekly tasks
 	"""
-	r = get_redis_conn()
+	purged_task_count = 0
+	for queue in get_queue_list(queue):
+		q = get_queue(queue)
+		for job in q.jobs:
+			if (site and event):
+				if job.kwargs['site'] == site and job.kwargs['event'] == event:
+					job.delete()
+					purged_task_count+=1
+			elif site:
+				if job.kwargs['site'] == site:
+					job.delete()
+					purged_task_count+=1
+			elif event:
+				if job.kwargs['event'] == event:
+					job.delete()
+					purged_task_count+=1
+			else:
+				purged_task_count += q.count
+				q.empty()
 
-	event_tasks = []
-	for app in frappe.get_all_apps(True):
-		all_events = frappe.get_hooks(app_name=app).get('scheduler_events', {})
-		if event:
-			event_tasks += all_events.get(event) or []
-		else:
-			# tasks of all events
-			for tasks in all_events.values():
-				event_tasks += tasks or []
 
-	count = 0
+	return purged_task_count
 
-	for queue in get_queues():
-		for taskstr in r.lrange(queue, 0, -1):
-			taskbody = get_task_body(taskstr)
-			kwargs = taskbody.get('kwargs')
-			if kwargs:
-				if site and kwargs.get('site') != site:
-					continue
+def get_jobs_by_queue(site=None):
+	jobs_per_queue = defaultdict(list)
+	job_count = consolidated_methods = {}
+	for queue in get_queue_list():
+		q = get_queue(queue)
+		for job in q.jobs:
+			if not site:
+				jobs_per_queue[queue].append(job.kwargs['method'])
+			elif job.kwargs['site'] == site:
+				jobs_per_queue[queue].append(job.kwargs['method'])
 
-				if event:
-					if kwargs.get('handler') and kwargs.get('handler') in event_tasks:
-						r.lrem(queue, taskstr)
-						count += 1
-				else:
-					r.lrem(queue, taskstr)
-					count += 1
-	return count
+		consolidated_methods = {}
 
-def get_pending_task_count():
-	"Get count of pending tasks"
-	r = get_redis_conn()
-	pending = 0
-	for queue in get_queues():
-		pending += r.llen(queue)
-	return pending
+		for method in jobs_per_queue[queue]:
+			if method not in consolidated_methods.keys():
+				consolidated_methods[method] = 1
+			else:
+				consolidated_methods[method] += 1
 
-def get_timedout_locks():
-	"Get list of stale locks from all sites"
-	old_locks=[]
-	for site in frappe.utils.get_sites():
-		locksdir = os.path.join(site, 'locks')
-		for lock in os.listdir(locksdir):
-			lock_path = os.path.join(locksdir, lock)
-			try:
-				check_lock(lock_path)
-			except LockTimeoutError:
-				old_locks.append(lock_path)
-	return old_locks
+		job_count[queue] = len(jobs_per_queue[queue])
+		jobs_per_queue[queue] = consolidated_methods
+		
 
-def check_if_workers_online():
-	app = get_celery()
-	if app.control.inspect().ping():
-		return True
-	return False
+	return jobs_per_queue, job_count
 
-def dump_queue_status(site=None):
-	"""
-	Dumps pending events and tasks per queue
-	"""
-	ret = []
-	r = get_redis_conn()
-	for queue in get_queues(site=site):
-		if r.llen(queue):
-			queue_details = {
-				'queue': queue,
-				'len': r.llen(queue),
-			}
-			queue_details.update(get_task_count_for_queue(queue))
-			ret.append(queue_details)
 
-	ret = sorted(ret, key=itemgetter('len'), reverse=True)
-	ret.insert(0, {
-		'total': get_pending_task_count()
-	})
-	return ret
+def get_pending_jobs(site=None):
+	jobs_per_queue = defaultdict(list)
+	for queue in get_queue_list():
+		q = get_queue(queue)
+		for job in q.jobs:
+			method_kwargs = job.kwargs['kwargs'] if job.kwargs['kwargs'] else ""
+			if job.kwargs['site'] == site:
+				jobs_per_queue[queue].append("{0} {1}".
+					format(job.kwargs['method'], method_kwargs))
 
-def get_task_count_for_queue(queue):
-	"""
-	For a given queue, returns the count of every pending task and aggregate of
-	events pending
-	"""
-	r = get_redis_conn()
-	tasks = [get_task_body(taskstr) for taskstr in r.lrange(queue, 0, -1)]
-	task_names = [task['task'] for task in tasks]
-	task_counts = Counter(task_names)
-	event_counts = Counter(task['kwargs'].get('event') for task in tasks)
-	return {
-		'task_counts': task_counts,
-		'event_counts': event_counts
-	}
+	return jobs_per_queue
 
+
+
+def check_number_of_workers():
+	return len(get_workers())
+	
 def get_running_tasks():
-	ret = {}
-	app = get_celery()
-	inspect = app.control.inspect()
-	active = inspect.active()
-	if not active:
-		return []
-	for worker in active:
-		ret[worker] = []
-		for task in active[worker]:
-			ret[worker].append({
-				'id': task['id'],
-				'name': task['name'],
-				'routing_key': task['delivery_info']['routing_key'],
-				'args': task['args'],
-				'kwargs': task['kwargs']
-			})
-	return ret
+	for worker in get_workers():
+		return worker.get_current_job()
 
 
-def doctor():
+def doctor(site=None):
 	"""
 	Prints diagnostic information for the scheduler
 	"""
-	print "Inspecting workers and queues..."
-	workers_online = check_if_workers_online()
-	pending_tasks = get_pending_task_count()
-
-	print "Finding locks..."
-	locks = get_timedout_locks()
-
-	print "Checking scheduler status..."
+	workers_online = check_number_of_workers()
+	jobs_per_queue, job_count = get_jobs_by_queue(site)
+	print "-----Checking scheduler status-----"
 	for site in frappe.utils.get_sites():
 		frappe.init(site)
 		frappe.connect()
 		if is_scheduler_disabled():
-			print "{0:40}: Scheduler disabled via System Settings or site_config.json".format(site)
+			print "Scheduler disabled for : "
 		frappe.destroy()
 
 	print "Workers online:", workers_online
-	print "Pending tasks", pending_tasks
-	print "Timed out locks:"
-	print "\n".join(locks)
-	if (not workers_online) or (pending_tasks > 4000) or locks:
-		return 1
+	print "-----{0} Jobs-----".format(site)
+	for queue in get_queue_list():
+		if jobs_per_queue[queue]:
+			print "Queue:", queue
+			print "Number of Jobs: ", job_count[queue]
+			print "Methods:"
+			for method, count in jobs_per_queue[queue].iteritems():
+				print "{0} : {1}".format(method, count)
+			print "------------"
 
-	print "Note: To view pending tasks, use bench dump-queue-status"
 
 	return True
 
-def celery_doctor(site=None):
-	queues = dump_queue_status(site=site)
-	running_tasks = get_running_tasks()
-	print 'Queue Status'
-	print '------------'
-	print json.dumps(queues, indent=1)
-	print ''
-	print 'Running Tasks'
-	print '------------'
-	print json.dumps(running_tasks, indent=1)
-
-def inspect_queue():
-	print 'Pending Tasks Queue'
-	print '-'*20
-	r = get_redis_conn()
-	for queue in get_queues():
-		for taskstr in r.lrange(queue, 0, -1):
-			taskbody = get_task_body(taskstr)
-			kwargs = taskbody.get('kwargs')
-			if kwargs:
-				print frappe.as_json(kwargs)
+def pending_jobs(site=None):
+	print "-----Pending Jobs-----"
+	pending_jobs = get_pending_jobs(site)
+	for queue in get_queue_list():
+		if(pending_jobs[queue]):
+			print "-----Queue :{0}-----".format(queue)
+			print "\n".join(pending_jobs[queue])
+	