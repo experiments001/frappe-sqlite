experiments001/frappe-sqlite

commit 0a4bc4b72f1acff7a10dfa4783b301991e70894f

Update api.py

frappe/api.py
@@ -81,7 +81,10 @@ def handle():
 					frappe.local.response.update({"data": doc})
 
 				if frappe.local.request.method=="PUT":
-					data = json.loads(frappe.local.form_dict.data)
+					if frappe.local.form_dict.data is None:
+						data = json.loads(frappe.local.request.get_data())
+					else:
+						data = json.loads(frappe.local.form_dict.data)
 					doc = frappe.get_doc(doctype, name)
 
 					if "flags" in data:

commit f00928fb9cffa8423ce9ecb60c0b3fe09b6b0f42

fix: REST API utf-8 decoding on creates/updates (#8875)
* fix: REST API utf-8 decoding on creates/updates

Creating or updating a document via the REST API would generate an error of:

`TypeError: the JSON object must be str, not 'bytes'`

Because get_data() returns bytes which must be explicitly converted to a string before parsing as JSON.

Defect introduced by 0a4bc4b72 and 5af987e73b

* fix: switch to safe_decode

frappe/api.py
@@ -82,7 +82,7 @@ def handle():
 
 				if frappe.local.request.method=="PUT":
 					if frappe.local.form_dict.data is None:
-						data = json.loads(frappe.local.request.get_data())
+						data = json.loads(frappe.safe_decode(frappe.local.request.get_data()))
 					else:
 						data = json.loads(frappe.local.form_dict.data)
 					doc = frappe.get_doc(doctype, name)
@@ -117,7 +117,7 @@ def handle():
 
 				if frappe.local.request.method=="POST":
 					if frappe.local.form_dict.data is None:
-						data = json.loads(frappe.local.request.get_data())
+						data = json.loads(frappe.safe_decode(frappe.local.request.get_data()))
 					else:
 						data = json.loads(frappe.local.form_dict.data)
 					data.update({