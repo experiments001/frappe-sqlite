frappe/frappe

feat(utm): make data helper more useful

frappe/utils/data.py
@@ -2516,9 +2516,21 @@ def add_trackers_to_url(
 	medium: str | None = None,
 	content: str | None = None,
 ) -> str:
+	def get_utm_values():
+		_source = frappe.db.get_value("Marketing Source", source, "slug") or source
+		_campaign = frappe.db.get_value("Marketing Campaign", campaign, "slug") or campaign
+		_medium = frappe.db.get_value("Marketing Medium", medium, "slug") or medium
+		return _source, _medium, _campaign
+
 	url_parts = list(urlparse(url))
 	if url_parts[0] == "mailto":
 		return url
+
+	source, medium, campaign = frappe.cache.get_value(
+		"utm_" + cstr(source) + "_" + cstr(medium) + "_" + cstr(campaign),
+		get_utm_values,
+		shared=True,
+	)
 
 	trackers = {"utm_source": source}
 
@@ -2535,6 +2547,52 @@ def add_trackers_to_url(
 
 	url_parts[4] = urlencode(query)
 	return urlunparse(url_parts)
+
+
+def parse_and_map_trackers_from_url(url: str, create: bool = False) -> dict:
+	query = urlparse(url).query
+
+	url_trackers = dict(parse_qsl(query))
+
+	return map_trackers(url_trackers, create)
+
+
+def map_trackers(url_trackers: dict, create: bool = False):
+	frappe_trackers = {}
+
+	if url_source := url_trackers.get("utm_source", url_trackers.get("source")):
+		source = frappe.db.get_value("Marketing Source", {"slug": slug(url_source)}, "name") or url_source
+		if create and source == url_source and not frappe.db.exists("Marketing Source", source):
+			source = frappe.new_doc("Marketing Source")
+			source.name = url_source
+			source.description = f"Autogenerated from {url_trackers}"
+			source.save(ignore_permissions=True)
+		frappe_trackers["utm_source"] = source
+
+	if url_medium := url_trackers.get("utm_medium", url_trackers.get("medium")):
+		medium = frappe.db.get_value("Marketing Medium", {"slug": slug(url_medium)}, "name") or url_medium
+		if create and medium == url_medium and not frappe.db.exists("Marketing Medium", medium):
+			medium = frappe.new_doc("Marketing Medium")
+			medium.name = url_medium
+			medium.description = f"Autogenerated from {url_trackers}"
+			medium.save(ignore_permissions=True)
+		frappe_trackers["utm_medium"] = medium
+
+	if url_campaign := url_trackers.get("utm_campaign", url_trackers.get("campaign")):
+		campaign = (
+			frappe.db.get_value("Marketing Campaign", {"slug": slug(url_campaign)}, "name") or url_campaign
+		)
+		if create and campaign == url_campaign and not frappe.db.exists("Marketing Campaign", campaign):
+			campaign = frappe.new_doc("Marketing Campaign")
+			campaign.name = url_campaign
+			campaign.campaign_description = f"Autogenerated from {url_trackers}"
+			campaign.save(ignore_permissions=True)
+		frappe_trackers["utm_campaign"] = campaign
+
+	if url_content := url_trackers.get("utm_content", url_trackers.get("content")):
+		frappe_trackers["utm_content"] = url_content
+
+	return frappe_trackers
 
 
 # This is used in test to count memory overhead of default imports.

experiments001/frappe-sqlite

frappe/utils/data.py
@@ -2516,9 +2516,21 @@ def add_trackers_to_url(
 	medium: str | None = None,
 	content: str | None = None,
 ) -> str:
+	def get_utm_values():
+		_source = frappe.db.get_value("UTM Source", source, "slug") or source
+		_campaign = frappe.db.get_value("UTM Campaign", campaign, "slug") or campaign
+		_medium = frappe.db.get_value("UTM Medium", medium, "slug") or medium
+		return _source, _medium, _campaign
+
 	url_parts = list(urlparse(url))
 	if url_parts[0] == "mailto":
 		return url
+
+	source, medium, campaign = frappe.cache.get_value(
+		"utm_" + cstr(source) + "_" + cstr(medium) + "_" + cstr(campaign),
+		get_utm_values,
+		shared=True,
+	)
 
 	trackers = {"utm_source": source}
 
@@ -2535,6 +2547,50 @@ def add_trackers_to_url(
 
 	url_parts[4] = urlencode(query)
 	return urlunparse(url_parts)
+
+
+def parse_and_map_trackers_from_url(url: str, create: bool = False) -> dict:
+	query = urlparse(url).query
+
+	url_trackers = dict(parse_qsl(query))
+
+	return map_trackers(url_trackers, create)
+
+
+def map_trackers(url_trackers: dict, create: bool = False):
+	frappe_trackers = {}
+
+	if url_source := url_trackers.get("utm_source", url_trackers.get("source")):
+		source = frappe.db.get_value("UTM Source", {"slug": slug(url_source)}, "name") or url_source
+		if create and source == url_source and not frappe.db.exists("UTM Source", source):
+			source = frappe.new_doc("UTM Source")
+			source.name = url_source
+			source.description = f"Autogenerated from {url_trackers}"
+			source.save(ignore_permissions=True)
+		frappe_trackers["utm_source"] = source
+
+	if url_medium := url_trackers.get("utm_medium", url_trackers.get("medium")):
+		medium = frappe.db.get_value("UTM Medium", {"slug": slug(url_medium)}, "name") or url_medium
+		if create and medium == url_medium and not frappe.db.exists("UTM Medium", medium):
+			medium = frappe.new_doc("UTM Medium")
+			medium.name = url_medium
+			medium.description = f"Autogenerated from {url_trackers}"
+			medium.save(ignore_permissions=True)
+		frappe_trackers["utm_medium"] = medium
+
+	if url_campaign := url_trackers.get("utm_campaign", url_trackers.get("campaign")):
+		campaign = frappe.db.get_value("UTM Campaign", {"slug": slug(url_campaign)}, "name") or url_campaign
+		if create and campaign == url_campaign and not frappe.db.exists("UTM Campaign", campaign):
+			campaign = frappe.new_doc("UTM Campaign")
+			campaign.name = url_campaign
+			campaign.campaign_description = f"Autogenerated from {url_trackers}"
+			campaign.save(ignore_permissions=True)
+		frappe_trackers["utm_campaign"] = campaign
+
+	if url_content := url_trackers.get("utm_content", url_trackers.get("content")):
+		frappe_trackers["utm_content"] = url_content
+
+	return frappe_trackers
 
 
 # This is used in test to count memory overhead of default imports.